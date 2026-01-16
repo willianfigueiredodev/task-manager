830a74fbd7306cef377be578ca06ea4b6c267e42
Willian Figueiredo <willianfigueiredodev@gmail.com>
2026-01-16 18:52:43 -0300

    feat: updated main to use modular architecture

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from config.database import engine, Base
+from routers import task_router
+import models 
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(task_router.router)
+
 @app.get("/")
-def read_root():
-    return {"message": "Ambiente Configurado com Sucesso!"}
+def health_check():
+    return {
+        "status": "online",
+        "message": "Task Manager API rodando com Arquitetura Modular! 🚀"
+    }
