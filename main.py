734a7f7b8b4692ff1c66fd8aef75b84655e05614
Willian Figueiredo <willianfigueiredodev@gmail.com>
2026-01-17 00:05:34 -0300

    fix: configured CORS for frontend integration

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config.database import engine, Base
 from routers import task_router
-import models 
+import models
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
 
 app.include_router(task_router.router)
 
@@ -13,5 +26,5 @@ app.include_router(task_router.router)
 def health_check():
     return {
         "status": "online",
-        "message": "Task Manager API rodando com Arquitetura Modular! 🚀"
+        "message": "Task Manager API rodando com CORS Liberado! 🚀"
     }
